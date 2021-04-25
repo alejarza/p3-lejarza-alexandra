d8fd4bf58ef76cedf07c6881d0617d5d75ed1517
Alexandra Lejarza <alejarza@ufl.edu>
2021-04-25 19:11:52 -0400

    added form folder and code

---
+++ form/default.inc.php
@@ -0,0 +1,108 @@
+<style type="text/css">
+* {
+  padding: 0;
+  margin: 0;
+  border: 0;
+  font-family: 'Lato', sans-serif;
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 900px;
+  margin: 50px auto 180px;
+  text-align: left;
+  position: relative;
+  top: 200px;
+}
+
+header {
+  background: #a7869e;
+  text-align: center;
+  position: fixed;
+  width: 100%;
+  z-index: 99999;
+}
+
+header nav li {
+  display: inline-block;
+  max-width: 200px;
+}
+
+header nav a {
+  text-decoration: none;
+  color: white;
+  margin: 0 20px;
+  font-size: 20px;
+  text-align: center;
+}
+
+footer {
+  background: white;
+  text-align: center;
+  position: fixed;
+  width: 100%;
+  bottom: 0;
+  padding: 15px;
+  z-index: 99999;
+}
+
+footer nav li {
+  display: inline-block;
+  max-width: 200px;
+}
+
+footer nav a {
+  text-decoration: none;
+  color: #48546f;
+  margin: 0 20px;
+  font-size: 16px;
+  text-align: center;
+}
+
+h1,
+p {
+  color: #48546f;
+  text-align: center;
+}
+
+h1 {
+  font-size: 40px;
+  padding: 0 0 32px;
+  margin: 100px 0 20px;
+}
+
+p {
+  max-width: 500px;
+  margin: 0 auto;
+  padding-bottom: 75px;
+}
+
+</style>
+
+<header>
+  <nav>
+    <ul>
+      <li><img src="images/logo.png" width="150" alt="logo"></li>
+      <li><a href="index.html">Home</a></li>
+      <li><a href="experience.html">Experience</a></li>
+      <li><a href="portfolio.html">Design Portfolio</a></li>
+      <li><a href="contact.html">Contact Me</a></li>
+    </ul>
+  </nav>
+</header>
+
+<div class="container">
+  <h1>Sorry something went wrong with your submission</h1>
+  <p>Please try again</p>
+</div>
+
+<footer>
+  <nav>
+    <ul>
+      <li><a href="index.html">Home</a></li>
+      <li><a href="experience.html">Experience</a></li>
+      <li><a href="portfolio.html">Design Portfolio</a></li>
+      <li><a href="contact.html">Contact Me</a></li>
+    </ul>
+  </nav>
+</footer>
